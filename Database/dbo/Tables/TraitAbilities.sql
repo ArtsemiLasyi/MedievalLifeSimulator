fbf5611a8dbafe1e682b6a90a9f113c35ff6b11f
ArtsemiLasyi <50248368+ArtsemiLasyi@users.noreply.github.com>
2021-11-29 19:01:56 +0300

    Last updates

---
+++ Database/dbo/Tables/TraitAbilities.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[TraitAbilities] (
+    [TraitId]         INT NOT NULL,
+    [BonusMartial]    INT NOT NULL,
+    [BonusIntrigue]   INT NOT NULL,
+    [BonusManagement] INT NOT NULL,
+    [BonusDiplomacy]  INT NOT NULL,
+    [BonusEducation]  INT NOT NULL,
+    CONSTRAINT [PK_TraitAbilities] PRIMARY KEY CLUSTERED ([TraitId] ASC),
+    CONSTRAINT [FK_TraitAbilities_Traits] FOREIGN KEY ([TraitId]) REFERENCES [dbo].[Traits] ([Id])
+);
+
